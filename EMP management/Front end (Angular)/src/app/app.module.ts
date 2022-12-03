7c630c0186f9bfc00ea5b1cce07da554d0b02e67
Chinnaiah-N <119689664+Chinnaiah-N@users.noreply.github.com>
2022-12-03 21:01:08 +0530

    Update app.module.ts

--- EMP management/Front end (Angular)/src/app/app.module.ts
+++ EMP management/Front end (Angular)/src/app/app.module.ts
@@ -1,23 +1,45 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { authInterceptorProviders } from './_helper/auth.interceptor';
+import { ProfileComponent } from './profile/profile.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { HomeloginComponent } from './homelogin/homelogin.component';
+
+//import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    BoardUserComponent,
+    BoardModeratorComponent,
+    BoardAdminComponent
+    
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
