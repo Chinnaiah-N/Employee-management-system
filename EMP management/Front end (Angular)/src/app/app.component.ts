8569366a2ff2df106567e3923733783aa0eb4261
Chinnaiah-N <119689664+Chinnaiah-N@users.noreply.github.com>
2022-12-03 21:00:14 +0530

    Update app.component.ts

--- EMP management/Front end (Angular)/src/app/app.component.ts
+++ EMP management/Front end (Angular)/src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { TokenStorageService } from './service/token-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'employee-management-system';
+
+  private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username?: string;
+
+  constructor(private tokenStorageService: TokenStorageService) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+      this.username = user.username;
+    }
+  }
+
+  logout(): void {
+    this.tokenStorageService.signOut();
+    window.location.reload();
+  }
+
+  /*constructor(private http: HttpClient) {}
+
+  download(){
+    this.http.get('http://localhost:4200/employee/export/pdf', {responseType: 'arrayBuffer'}).subscribe(pdf => 
+    {
+      const blob= new Blob([pdf], [type: 'application/pdf']);
+      const filename="Employee.pdf";
+    }, err => {
+      console.log(err);
+
+    })
+  }*/
 }
